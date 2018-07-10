463374af14f1c4171d8a35e1214abbf719ed4784
DESKTOP-FLT18BS\chenzifeng <704734862@qq.com>
2018-07-10 14:52:18 +0800

    增加手机号检测的方法

--- src/main/java/com/xueqi/Intelligent_office/service/Judge.java
+++ src/main/java/com/xueqi/Intelligent_office/service/Judge.java
@@ -1,20 +1,39 @@
 package com.xueqi.Intelligent_office.service;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import java.text.SimpleDateFormat;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Judge {
-
+    private static Log log = LogFactory.getLog("Judge:");
     public static boolean timeOnDay(long aim, long currnent) {
         SimpleDateFormat s = new SimpleDateFormat("yyyy-MM-dd");
         String day1 = s.format(aim);
         String day2 = s.format(currnent);
-        if (day1.equals(day2))
-            return true;
-        else
-            return false;
+        return day1.equals(day2);
     }
 
     public static boolean numRangeJ(int current, int max, int min) {
         return Math.max(current, min) == Math.min(current, max);
+    }
+
+    public static boolean isPhone(String phone) {
+        String regex = "^((13[0-9])|(14[5,7,9])|(15([0-3]|[5-9]))|(17[0,1,3,5,6,7,8])|(18[0-9])|(19[8|9]))\\d{8}$";
+        if (phone.length() != 11) {
+            log.error("手机号位数错误");
+            return false;
+        } else {
+            Pattern p = Pattern.compile(regex);
+            Matcher m = p.matcher(phone);
+            boolean isMatch = m.matches();
+
+            if (!isMatch) {
+               log.error("请填入正确的手机号");
+            }
+            return isMatch;
+        }
     }
 }
--- src/test/java/com/xueqi/Intelligent_office/DepartmentTest.java
+++ src/test/java/com/xueqi/Intelligent_office/DepartmentTest.java
@@ -10,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.text.SimpleDateFormat;
+
 
 @SpringBootTest
 public class DepartmentTest {
@@ -18,6 +20,21 @@ public class DepartmentTest {
 //
     @Test
     public void test(){
-        System.out.println(Judge.timeOnDay(1528423820676L,1527242086043L));
+        SimpleDateFormat s = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        long aim = 1528423820676L ;
+        long currnent = 1527242086043L;
+
+        String day1 = s.format(aim);
+        String day2 = s.format(currnent);
+        System.out.println(day1);
+        System.out.println(day2);
+        System.out.println(Judge.timeOnDay(aim,currnent));
+    }
+
+    @Test
+    public void phoneTest(){
+        String phone = "1780620750";
+        System.out.println("输入为："+phone);
+        System.out.println("输出为：" +Judge.isPhone(phone));
     }
 }
